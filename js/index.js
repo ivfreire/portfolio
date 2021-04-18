857f618870b4abf2287f89682f285a8de53fc502
Icaro Freire <icarovf@usp.br>
2021-04-18 17:57:17 -0300

    updated: now on github pages

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Created by Ícaro Freire on April 16th 2021.
+
+window.onload = function() {
+	setTimeout(function() {
+		$('div.intro div.hide').slideDown(250);
+	}, 250);
+}
+
+window.onscroll = function() {
+	
+}
